94d1db986272ffad251317612da4c10ca143618b
Grq2019 <52628907+Grq2019@users.noreply.github.com>
2020-08-05 15:29:37 +0800

    Add files via upload

---
+++ Python初级/@property的使用.py
@@ -0,0 +1,31 @@
+class Screen(object):
+    @property
+    def width(self):
+        return self._width
+    @width.setter
+    def width(self,value):
+        if not isinstance(value,int):
+            raise ValueError('width must be integer')
+        self._width = value
+    
+    @property
+    def height(self):
+        return self._height
+    @height.setter
+    def height(self,value):
+        if not isinstance(value,int):
+            raise ValueError('width must be integer')
+        self._height = value
+    @property
+    def resolution(self):
+        return self._width*self._height
+
+# 测试:
+s = Screen()
+s.width = 1024
+s.height = 768
+print('resolution =', s.resolution)
+if s.resolution == 786432:
+    print('测试通过!')
+else:
+    print('测试失败!')
